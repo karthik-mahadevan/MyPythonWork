e93d36550a8fa72e52e2506435bce4ba69631915
karthik-mahadevan <ranjani@Ranjanis-MacBook-Pro.local>
2013-11-26 11:24:16 +0530

    new file

---
+++ Nested_Dictionaries_Experimentation 1100 2013-11-26.py
@@ -0,0 +1,12 @@
+user_list = ["user 1", "user 2"]
+service_list = ["web marketing", "SEO", "data analytics"]
+user_score_factors = {"user 1" : {"web marketing" : 2, "SEO" : 0.5}, "user 2" : {"web marketing" : 3, "SEO" : 4}}
+
+
+for user in user_list:
+	for service in service_list:
+		try : 
+			print user, service, user_score_factors[user][service] 
+		except : 
+			user_score_factors[user][service] = 1
+			print user_score_factors[user][service] 
